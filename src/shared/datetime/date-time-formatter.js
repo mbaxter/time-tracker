9c2b01bca89bffbb6adde64ddc843dcfacaf2cfe
Meredith Baxter <meredith.a.baxter@gmail.com>
2016-10-27 16:28:25 -0400

    Add DateTimeFormatter and test.

---
+++ src/shared/datetime/date-time-formatter.js
@@ -0,0 +1,69 @@
+/**
+ * DateTimeFormatter handles transforming datetime data from server-side ISO string to a display-friendly format
+ * Also handles taking in date & time data in various formats and normalizing to an ISO string
+ * @todo - Add isValidForDisplay() and isValidNormalizedValue() methods and tests.  Update tests to use these utilities
+ */
+"use strict";
+
+const moment = require('moment');
+require('moment-timezone');
+const TimeFormatter = require('./time-formatter');
+const DateFormatter = require('./date-formatter');
+
+const DateTimeFormatter = {};
+DateTimeFormatter.Time = TimeFormatter;
+DateTimeFormatter.Date = DateFormatter;
+DateTimeFormatter.dateTimeFormat = `${DateFormatter.internalFormat}T${TimeFormatter.internalFormat}`;
+DateTimeFormatter.internalFormat = `${DateTimeFormatter.dateTimeFormat}Z`;
+// Regex for splitting internal ISO string format into date, time, UTC offset components
+DateTimeFormatter.splitRegex = /(\d{4}-\d\d-\d\d)T(\d\d:\d\d:\d\d)([+-]\d\d:\d\d)/;
+
+/**
+ * Formats a normalized ISO string into date & time components for display
+ * @param {string} value A normalized ISO datetime string
+ * @param {string} timezone The user's timezone - see moment.tz.names()
+ * @param {boolean} includeYearForDate - Whether to format the date component with a year
+ * @return {Object} Return an object with 'date' and 'time' keys containing formatted strings for display
+ */
+DateTimeFormatter.parseForDisplay = function(value, timezone, includeYearForDate = false) {
+    // Reformat time in user's timezone
+    value = moment.tz(value, DateTimeFormatter.internalFormat, true, 'UTC').tz(timezone).format(DateTimeFormatter.internalFormat);
+    // Split ISO string into date, time and UTC offset components
+    const components = value.match(DateTimeFormatter.splitRegex);
+    if(!components || components.length < 3 || !moment.tz.zone(timezone)) {
+        return {};
+    }
+
+    return {
+        date: DateFormatter.formatForDisplay(components[1], includeYearForDate),
+        time: TimeFormatter.formatForDisplay(components[2])
+    };
+};
+
+/**
+ * Normalize and combine date and time input to a single ISO string in UTC+0 time
+ * @param {string} date A date string in various possible formats
+ * @param {string} time A time string in various possible formats
+ * @param {string} timezone The user's timezone. See moment-timezone.tz.names()
+ * @return {string} ISO datetime string in UTC+0 tz
+ */
+DateTimeFormatter.normalize = function(date, time, timezone) {
+    if (!moment.tz.zone(timezone)) {
+        return "Invalid date";
+    }
+
+    date = DateFormatter.normalize(date);
+    time = TimeFormatter.normalize(time);
+    return moment.tz(`${date}T${time}`, DateTimeFormatter.dateTimeFormat, true, timezone).tz('UTC').format(DateTimeFormatter.internalFormat);
+};
+
+/**
+ * Transform a javascript date object to a normalized ISO string in UTC+0 time
+ * @param {Date} date
+ * @return {string} ISO datetime string in UTC+0 tz
+ */
+DateTimeFormatter.normalizeDate = function(date) {
+    return moment.tz(date, 'UTC').format(DateTimeFormatter.internalFormat);
+};
+
+module.exports = DateTimeFormatter;
---
+++ test/unit/shared/datetime/date-time-formatter-test.js
@@ -0,0 +1,177 @@
+"use strict";
+const assert = require('assert');
+const DateTimeFormatter = require('../../../../src/shared/datetime/date-time-formatter');
+
+describe("DateTimeFormatter", () => {
+    describe("normalize", () => {
+        describe("with valid input", () => {
+            const testCases = [
+                {
+                    expected: "2016-10-27T19:23:00+00:00",
+                    input: ["Oct 27, 2016", "15:23", "US/Eastern"]
+                }
+            ];
+
+            testCases.forEach((testCase) => {
+                describe(`of ${JSON.stringify(testCase.input)}`, () => {
+                    it("should return a normalized ISO string with UTC offset +00:00", () => {
+                        const actual = DateTimeFormatter.normalize.apply(undefined, testCase.input);
+                        assert.equal(actual, testCase.expected);
+                    });
+                });
+            });
+        });
+
+        describe("with invalid input", () => {
+            describe("for date", () => {
+                const testCases = [
+                    {
+                        expected: "Invalid date",
+                        input: ["2016-22-55", "15:23", "US/Eastern"]
+                    }
+                ];
+
+                testCases.forEach((testCase) => {
+                    describe(`of ${JSON.stringify(testCase.input[0])}`, () => {
+                        it("should return 'Invalid date'", () => {
+                            const actual = DateTimeFormatter.normalize.apply(undefined, testCase.input);
+                            assert.equal(actual, 'Invalid date');
+                        });
+                    });
+                });
+            });
+            describe("for time", () => {
+                const testCases = [
+                    {
+                        expected: "Invalid date",
+                        input: ["2016-12-02", "55:23", "US/Eastern"]
+                    }
+                ];
+
+                testCases.forEach((testCase) => {
+                    describe(`of ${JSON.stringify(testCase.input[1])}`, () => {
+                        it("should return 'Invalid date'", () => {
+                            const actual = DateTimeFormatter.normalize.apply(undefined, testCase.input);
+                            assert.equal(actual, 'Invalid date');
+                        });
+                    });
+                });
+            });
+            describe("for timezone", () => {
+                const testCases = [
+                    {
+                        expected: "Invalid date",
+                        input: ["2016-22-55", "15:23", "bla"]
+                    }
+                ];
+
+                testCases.forEach((testCase) => {
+                    describe(`of ${JSON.stringify(testCase.input[2])}`, () => {
+                        it("should return 'Invalid date'", () => {
+                            const actual = DateTimeFormatter.normalize.apply(undefined, testCase.input);
+                            assert.equal(actual, 'Invalid date');
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+    describe("normalizeDate", () => {
+        describe("with valid input", () => {
+            const testCases = [
+                {
+                    expected: "1970-01-01T00:00:00+00:00",
+                    input: [(new Date(0))]
+                }
+            ];
+
+            testCases.forEach((testCase) => {
+                describe(`of ${JSON.stringify(testCase.input)}`, () => {
+                    it("should return a normalized ISO string with UTC offset +00:00", () => {
+                        const actual = DateTimeFormatter.normalizeDate.apply(undefined, testCase.input);
+                        assert.equal(actual, testCase.expected);
+                    });
+                });
+            });
+        });
+    });
+
+    describe("parseForDisplay", () => {
+        describe("with valid input", () => {
+            describe("and default 'includeYearForDate' parameter", () => {
+                const testCases = [
+                    {
+                        input: ["2016-10-27T19:23:00+00:00", "US/Eastern"],
+                        expected: {date: "Thu, Oct 27", time: "3:23 pm"}
+                    }
+                ];
+
+                testCases.forEach((testCase) => {
+                    describe(`of ${JSON.stringify(testCase.input)}`, () => {
+                        it("should return a normalized ISO string with UTC offset +00:00", () => {
+                            const actual = DateTimeFormatter.parseForDisplay.apply(undefined, testCase.input);
+                            assert.deepEqual(actual, testCase.expected);
+                        });
+                    });
+                });
+            });
+
+            describe("and 'includeYearForDate' parameter set to true", () => {
+                const testCases = [
+                    {
+                        input: ["2016-10-27T19:23:00+00:00", "US/Eastern", true],
+                        expected: {date: "Thu, Oct 27 2016", time: "3:23 pm"}
+                    }
+                ];
+
+                testCases.forEach((testCase) => {
+                    describe(`of ${JSON.stringify(testCase.input)}`, () => {
+                        it("should return a normalized ISO string with UTC offset +00:00", () => {
+                            const actual = DateTimeFormatter.parseForDisplay.apply(undefined, testCase.input);
+                            assert.deepEqual(actual, testCase.expected);
+                        });
+                    });
+                });
+            });
+        });
+
+        describe("with invalid input", () => {
+            describe("for the value parameter", () => {
+                const testCases = [
+                    {
+                        input: ["2016-22-27T19:23:00+00:00", "US/Eastern"],
+                        expected: {}
+                    }
+                ];
+
+                testCases.forEach((testCase) => {
+                    describe(`of ${JSON.stringify(testCase.input[0])}`, () => {
+                        it("should return a response indicating failure", () => {
+                            const actual = DateTimeFormatter.parseForDisplay.apply(undefined, testCase.input);
+                            assert.deepEqual(actual, testCase.expected);
+                        });
+                    });
+                });
+            });
+
+            describe("for the timezone parameter", () => {
+                const testCases = [
+                    {
+                        input: ["2016-22-27T19:23:00+00:00", "bla"],
+                        expected: {}
+                    }
+                ];
+
+                testCases.forEach((testCase) => {
+                    describe(`of ${JSON.stringify(testCase.input[1])}`, () => {
+                        it("should return a response indicating failure", () => {
+                            const actual = DateTimeFormatter.parseForDisplay.apply(undefined, testCase.input);
+                            assert.deepEqual(actual, testCase.expected);
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
