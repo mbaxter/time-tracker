c9e8079f991bf8e96c16e455e34717ad2c0bfef6
Meredith Baxter <meredith.a.baxter@gmail.com>
2016-11-17 14:50:57 -0500

    Add logout link.

--- src/frontend/components/stateful/app-nav-bar.js
+++ src/frontend/components/stateful/app-nav-bar.js
@@ -4,20 +4,27 @@ const NavBar = require('../presentational/nav-bar');
 const subject = require('../../selector/subject-selector');
 
 const timeTest = /^\/app(\/time-blocks.*)?$/;
+const publicPathTest = /^\/(login|signup)?$/;
 
 const mapStateToProps = (state, ownProps) => {
-   const authenticated = subject.authenticated(state);
+    const authenticated = subject.authenticated(state);
     const navItemsLeft = [];
     const navItemsRight = [];
 
     const currentPath = ownProps.location;
 
-    if (authenticated) {
-       navItemsLeft.push({
-           label: "Manage Time",
-           url: "/app/time-blocks",
-           active: timeTest.test(currentPath)
-       });
+    if (authenticated && !publicPathTest.test(currentPath)) {
+        navItemsLeft.push({
+            label: "Manage Time",
+            url: "/app/time-blocks",
+            active: timeTest.test(currentPath)
+        });
+
+        navItemsRight.push({
+            label: "Log out",
+            url: "/login",
+            active: false
+        });
     }
     return {
         navItemsLeft,
