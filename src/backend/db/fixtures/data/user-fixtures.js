ec1407026c1c121cc2bf5d3ab3cabd3fa2fea6fb
Meredith Baxter <meredith.a.baxter@gmail.com>
2016-11-17 19:35:22 -0500

    Genericize fixtures.

--- src/backend/db/fixtures/data/user-fixtures.js
+++ src/backend/db/fixtures/data/user-fixtures.js
@@ -3,16 +3,16 @@ const UserRole = require('../../../../shared/constants/user-role');
 
 module.exports = [
     {
-        first_name: "Angela",
-        last_name: "Lansbury",
+        first_name: "Joe",
+        last_name: "Admin",
         email_address: "admin@test.com",
         password: process.env.ADMIN_PASSWORD,
         role: UserRole.ADMIN,
         timezone: "US/Eastern"
     },
     {
-        first_name: "Fred",
-        last_name: "Astaire",
+        first_name: "Jane",
+        last_name: "Demo",
         email_address: "demo@test.com",
         password: "12345",
         role: UserRole.STANDARD,
