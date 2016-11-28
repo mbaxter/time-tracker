fd85179ce3ac40b50f23cdbd65227e75e8eee85d
Meredith Baxter <meredith.a.baxter@gmail.com>
2016-11-27 23:10:12 -0500

    Only show paging if more than one page exists.

--- src/frontend/components/presentational/datatable/add-ons/paging.js
+++ src/frontend/components/presentational/datatable/add-ons/paging.js
@@ -9,29 +9,33 @@ const Paging = (props) => {
     const nextPage = Math.min(props.currentPage + 1, props.totalPages || props.currentPage + 1);
     return (
         <div className="horizontal-spread">
+            {(props.totalPages > 1) &&
             <div className="horizontal-spread">
                 <div className="btn-group" role="group">
-                    <button key="firstPage" className="btn btn-default btn-sm" disabled = {props.currentPage == 1}
+                    <button key="firstPage" className="btn btn-default btn-sm" disabled={props.currentPage == 1}
                             onClick={() => props.onGoToPage(1)} aria-label="First Page">
                         <i className="fa fa-fast-backward"/> First
                     </button>
-                    <button key="previousPage" className="btn btn-default btn-sm" disabled = {props.currentPage == 1}
+                    <button key="previousPage" className="btn btn-default btn-sm" disabled={props.currentPage == 1}
                             onClick={() => props.onGoToPage(previousPage)} aria-label="Previous Page">
                         <i className="fa fa-step-backward"/> Previous
                     </button>
                 </div>
                 <p className="current-page">Page {props.currentPage} / {props.totalPages || <i>Calculating ...</i>}</p>
                 <div className="btn-group" role="group">
-                    <button key="nextPage" className="btn btn-default btn-sm" disabled = {props.currentPage == props.totalPages}
+                    <button key="nextPage" className="btn btn-default btn-sm"
+                            disabled={props.currentPage == props.totalPages}
                             onClick={() => props.onGoToPage(nextPage)} aria-label="Next Page">
                         Next <i className="fa fa-step-forward"/>
                     </button>
-                    <button key="lastPage" className="btn btn-default btn-sm" disabled = {props.currentPage == props.totalPages}
+                    <button key="lastPage" className="btn btn-default btn-sm"
+                            disabled={props.currentPage == props.totalPages}
                             onClick={() => props.onGoToPage(-1)} aria-label="Last Page">
                         Last <i className="fa fa-fast-forward"/>
                     </button>
                 </div>
             </div>
+            }
         </div>
     );
 };
