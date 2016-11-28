c7999cb309d9f0ef464e8119d42989bb20761a2f
Meredith Baxter <meredith.a.baxter@gmail.com>
2016-11-28 14:03:03 -0500

    Setup minimum range for chart if there’s a small amount of data.

--- src/frontend/selector/time-block-chart/bar-chart-range.js
+++ src/frontend/selector/time-block-chart/bar-chart-range.js
@@ -5,6 +5,8 @@ const first = require('lodash/first');
 const last = require('lodash/last');
 const RecordTypes = require('../../constants/record-types');
 const DateTimeFormatter = require('../../../shared/datetime/format/date-time-formatter');
+const TimeRangeFormatter = require('../../../shared/datetime/format/time-range-formatter');
+const DateFactory = require('../../../shared/datetime/factory/date-factory');
 const subjectSelectors = require('./../subject-selector/index');
 const get = require('lodash/get');
 
@@ -22,6 +24,13 @@ module.exports = createSelector(
 
         let maxTimeBlock = DateTimeFormatter.parse(get(first(timeBlocks), 'start', ''), timezone);
         let minTimeBlock = DateTimeFormatter.parse(get(last(timeBlocks), 'start',''), timezone);
+
+        // If we only have a small amount of data, extend the range
+        let minBreadth = 6;
+        let breadth = TimeRangeFormatter.getRangeInDays(minTimeBlock.date, maxTimeBlock.date);
+        if (breadth < minBreadth) {
+            minTimeBlock.date = DateFactory.increment(maxTimeBlock.date, -minBreadth);
+        }
 
         return {
             min: minTimeBlock.date,
