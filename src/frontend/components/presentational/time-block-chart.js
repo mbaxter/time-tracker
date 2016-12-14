c0e6aa8e567ce8dbec834e5c67a7e298835e80b6
Meredith Baxter <meredith.a.baxter@gmail.com>
2016-12-13 23:36:51 -0500

    Add hasData attribute back - it was just misspelled as hasDate before.

--- src/frontend/components/presentational/time-block-chart.js
+++ src/frontend/components/presentational/time-block-chart.js
@@ -157,6 +157,7 @@ class TimeBlockDataTable extends React.Component {
 
 TimeBlockDataTable.propTypes = {
     data: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
+    hasData: React.PropTypes.bool.isRequired,
     onCreate: React.PropTypes.func,
     onDelete: React.PropTypes.func.isRequired,
     onEdit: React.PropTypes.func.isRequired,
