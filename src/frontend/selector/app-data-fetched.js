65c15fe65850e2a8347fe72d5eb57dcfac8e11ff
Meredith Baxter <meredith.a.baxter@gmail.com>
2016-12-14 18:59:58 -0500

    Fix app-data-fetched selector to include user data if necessary.

--- src/frontend/selector/app-data-fetched.js
+++ src/frontend/selector/app-data-fetched.js
@@ -2,11 +2,15 @@
 const createSelector = require('../util/createSelector');
 const subjectSelectors = require('./subject-selector');
 const RecordTypes = require('../constants/record-types');
+const UserRole = require('../../shared/constants/user-role');
 
 module.exports = createSelector(
     subjectSelectors.currentUser,
     subjectSelectors.batchPull(RecordTypes.TIME_BLOCK),
-    (currentUser, batchPull) => {
-        return currentUser && batchPull.finished;
+    subjectSelectors.batchPull(RecordTypes.USER),
+    (currentUser, timeBlockBatchPull, userBatchPull) => {
+        return currentUser && timeBlockBatchPull.finished &&
+            // Admin users required user data to be pulled
+            (currentUser.role != UserRole.ADMIN || userBatchPull.finished);
     }
 );
